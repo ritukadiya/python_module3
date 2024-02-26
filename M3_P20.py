019e46bd9599a76434b1a50775ca7635f1102c00
ritukadiya <145090408+ritukadiya@users.noreply.github.com>
2024-02-26 19:54:37 +0530

    Add files via upload

---
+++ M3_P20.py
@@ -0,0 +1,4 @@
+#Write a Python program to create a tuple with numbers.
+t=(1,2,3,4,5)
+
+print(t)
---
+++ M3_P21.py
@@ -0,0 +1,10 @@
+#Write a Python program to convert a tuple to a string.
+t = ('ritu', 'kadiya',1)
+ 
+s = ''
+ 
+for i in t:
+    s += i
+ 
+print("Given Python Tuple : ", t)
+print("Generated Python String : ", s)
